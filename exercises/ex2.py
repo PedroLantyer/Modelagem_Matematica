0154ebf4efd2630424b7d0284830eeaa8719b6b0
44462576+PedroLantyer@users.noreply.github.com <pedrolantyer@hotmail.com>
2024-08-27 14:39:06 -0300

    Update ex2.py

--- exercises/ex2.py
+++ exercises/ex2.py
@@ -3,84 +3,118 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-def clearTerminal():
-    if os.name.upper() == "NT":
-        os.system("cls")
+class utils:
+    def clearTerminal():
+        if os.name.upper() == "NT":
+            os.system("cls")
 
-def f(x):
-    return (pow(x,2) + (1/x) - 5)
+    def printArr(arr):
+        try:
+            print("Iterations: ")
+            for i in range(len(arr)):
+                print(f"A: {arr[i][0]:.3f}, B: {arr[i][1]:.3f}, C: {arr[i][2]:.3f}")
+            print("\n")
+        except Exception as err:
+            print(err)
 
+class problem:
 
+    def __init__(self, intervalStart: float, intervalEnd: float, tolerance: float) -> None:
+        try:
+            if(type(intervalStart) != float and type(intervalStart) != int):
+                raise TypeError("Interval Start value must be Int or Float")
+            if(type(intervalEnd) != float and type(intervalEnd) != int):
+                raise TypeError("Interval End value must be Int or Float")
+            if(type(tolerance) != float and type(tolerance) != int):
+                raise TypeError("Tolerance value must be Int or Float")
+            
+            self.a = intervalStart
+            self.b = intervalEnd
+            self.tolerance = tolerance
+        
+        except Exception as err:
+            print(err)
 
-def bisection(f, a, b, tolerance):
-    if (f(a) * f(b) == 0):
-            print("Fail")
+    def f(self, x):
+        try:
+            return (pow(x,2) + (1/x) - 5)
+        
+        except Exception as err:
+            print(err)
             return None
-    
-    iterrationArr = []
 
-    c = (a + b) / 2.0
-    while(b - a) / 2.0 > tolerance:
-        iterrationArr.append((a, b, c))
-        if(f(c) == 0):
-            return c
-        elif(f(a) * f(c) < 0):
-            b = c
-        else:
-            a = c
+    def bisection(self):
+        try:
+            a, b, = self.a, self.b
+            if (self.f(a) * self.f(b) == 0):
+                raise Exception("Fail")
+            
+            iterrationArr = []
+            
 
-        c = (a + b) / 2.0
+            c = (a + b) / 2.0
+            while(b - a) / 2.0 > self.tolerance:
+                iterrationArr.append((a, b, c))
+                if(self.f(c) == 0):
+                    return c
+                elif(self.f(a) * self.f(c) < 0):
+                    b = c
+                else:
+                    a = c
 
-    return iterrationArr
+                c = (a + b) / 2.0
 
-def printArr(arr):
+            return iterrationArr
+        
+        except Exception as err:
+            print(err)
+            return None
+
+def drawGraph(problemObj:object, data: list, yLimit:int = None, animationTimer:int = 2000):
     try:
-        print("Iterations: ")
-        for i in range(len(arr)):
-            print(f"A: {arr[i][0]:.3f}, B: {arr[i][1]:.3f}, C: {arr[i][2]:.3f}")
-        print("\n")
+        # Prepare the plot
+        figure, ax = plt.subplots()
+        x = np.linspace(1e-3, 6, 200)
+
+        y = problemObj.f(x)
+        ax.plot(x, y, 'b-', label='f(x)')
+        ax.axhline(0, color='black', lw=0.5)
+        line, = ax.plot([], [], 'ro-', lw=2)
+        text = ax.text(0.5, 0.5, '', transform=ax.transAxes, ha='center')
+
+        # Set up the plot limits and labels
+        ax.set_xlim(0, 6) #Set limits for the X axis
+
+        #Set limits for the Y axis
+        if(yLimit == None):
+            ax.set_ylim(min(y), 30) 
+        else:
+            ax.set_ylim(min(y), max(y))
+
+        ax.set_xlabel('x') #Set label For X axis
+        ax.set_ylabel('f(x)') #Set label for Y axis
+        ax.legend()
+
+        # Animation update
+        def update(i):
+            a, b, c = data[i]
+            line.set_data([a, b], [problemObj.f(a), problemObj.f(b)])
+            text.set_text(f'Iteration {i+1}: c = {c:.3f}')
+            return line, text
+        
+        ani = FuncAnimation(figure, update, frames=len(data), interval=animationTimer, repeat=False)
+
+        plt.show()
+    
     except Exception as err:
         print(err)
 
-    
-
 if __name__ == "__main__":
-    clearTerminal()
-
-    #Define A, B and Tolerance
-    intervalStart = 1e-3
-    intervalEnd = 5
-    tolerance = 1e-3
+    utils.clearTerminal()
+    problemObj = problem(intervalStart=1e-3, intervalEnd=5, tolerance=1e-3)
 
     #Create an array that contains the values for each iteration
-    iterationArr = bisection(f=f, a=intervalStart, b=intervalEnd, tolerance=tolerance)
-    printArr(iterationArr)
+    iterationArr = problemObj.bisection()
+    utils.printArr(iterationArr)
 
-
-        # Prepare the plot
-    figure, ax = plt.subplots()
-    x = np.linspace(1e-4, 6, 200)
-
-    y = f(x)
-    ax.plot(x, y, 'b-', label='f(x)')
-    ax.axhline(0, color='black', lw=0.5)
-    line, = ax.plot([], [], 'ro-', lw=2)
-    text = ax.text(0.5, 0.5, '', transform=ax.transAxes, ha='center')
-
-    # Set up the plot limits and labels
-    ax.set_xlim(0, 6) #Set limits for the X axis
-    ax.set_ylim(min(y), 30) #Set limits for the Y axis || UNDER NORMAL CIRCUMSTANCES THE UPPER LIMIT SHOULD BE MAX(Y)
-    ax.set_xlabel('x') #Set label For X axis
-    ax.set_ylabel('f(x)') #Set label for Y axis
-    ax.legend()
-
-    # Animation update
-    def update(i):
-        a, b, c = iterationArr[i]
-        line.set_data([a, b], [f(a), f(b)])
-        text.set_text(f'Iteration {i+1}: c = {c:.3f}')
-        return line, text
-    
-    ani = FuncAnimation(figure, update, frames=len(iterationArr), interval=2000, repeat=False)
-
-    plt.show()
+    drawGraph(problemObj=problemObj, data=iterationArr)
